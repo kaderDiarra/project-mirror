b7a33079ed6b26449b9af80d506930e185153733
kaderDiarra <abdel-aziz-kader.diarra@epitech.eu>
2021-05-13 15:12:46 +0200

    remove all

--- backend/routes/trade.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const controllers = require('../controllers/trade/trade')
-const { tradingRules, validate } = require('../controllers/validator')
-const router = express.Router()
-const { authorization } = require('../middleware/authorization')
-
-router.post('/', tradingRules(), validate, authorization, controllers.trade)
-
-module.exports = router
